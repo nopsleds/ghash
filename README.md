97917db1dbb48fc5162d219e64cdbd345df5aeec
nopsleds <31888206+nopsleds@users.noreply.github.com>
2018-02-13 22:12:18 +0100

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ghash
-file hash command line tool in golang
+
+A simple command-line tool for computing hashes (MD5 and SHA256) of one or multiple input files.
+
+## Usage
+
+    $ ./ghash.exe LICENSE README.md
+    Hashes for file 'LICENSE':
+     - MD5    : bb44830860d51d7e68a2b7df1a5b0bfc
+     - SHA256 : 2a2c285ee5ee973dc673c79e0ebc378aa1d053440e3da606cc835b4cd4a71012
+
+    Hashes for file 'README.md':
+     - MD5    : 1d8c5917f81d1b63cd2dca95aeecdad6
+     - SHA256 : c347a1d56c602feff6841dd1fbdde6ad4c29a832cc92b7d6155884c50fa134cf
+
+
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"crypto/md5"
+	"crypto/sha256"
+	"io"
+	"hash"
+)
+
+func applyHash(f *os.File, hashName string, hash hash.Hash) {
+	f.Seek(0, 0)
+	io.Copy(hash, f)
+	fmt.Printf(" - %s : %x\n", hashName, hash.Sum(nil))
+}
+
+
+func processFile(filePath string) {
+
+	// open file
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("error while opening file: %v", err)
+		return
+	}
+
+	defer f.Close()
+
+	fmt.Printf("Hashes for file '%v':\n", filePath)
+	applyHash(f, "MD5   ", md5.New())
+	applyHash(f, "SHA256", sha256.New())
+	fmt.Println()
+}
+
+func main(){
+	for _,f := range os.Args[1:] {
+		processFile(f)
+	}
+}
